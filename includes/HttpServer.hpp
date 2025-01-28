3ef20bbb0730aabe024f81e474661ac5681714ae
Semen Ganiev <sganiev@3-F-2.42heilbronn.de>
2025-01-28 12:22:28 +0100

    added config characteristics(part2)

--- includes/HttpServer.hpp
+++ includes/HttpServer.hpp
@@ -2,6 +2,7 @@
 #define	HTTPSERVER_HPP
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 #include "Location.hpp"
@@ -22,6 +23,12 @@ class HttpServer
 		std::string	server_name; // server_name example.com;
 
 		/**
+		 * The first server for a host:port will be the default
+		 * for this host:port (subject pdf)
+		 */
+		bool		is_default;
+
+		/**
 		 * map:
 		 * 404 -> "./htmls/404.html"
 		 * 500 -> "./htmls/500.html"
@@ -34,6 +41,7 @@ class HttpServer
 		std::map<int, std::string>	error_pages;
 
 		// routing rules based on the requested URL (see NGINX)
+		// each location should be unique so maybe we should use std:set
 		std::vector<Location>		locations;
 
 		/**
--- includes/Location.hpp
+++ includes/Location.hpp
@@ -2,6 +2,7 @@
 #define LOCATION_HPP
 
 #include <string>
+#include <vector>
 
 /**
  * routing rules based on the requested URL (see NGINX)
@@ -10,16 +11,42 @@
 class Location
 {
 	/**
-	 * location = /errors/404.html {}
+	 * location /htmls/errors {}
 	 * 
-	 * /errors/404.html -> path
+	 * /htmls/errors -> route
 	 * 
 	 */
 	private:
-		bool		is_internal;
+		bool		is_internal; // (?)
 		std::string	path;
 		std::string	root;
-		std::string alias; // (?)
+		std::string alias;
+
+		/**
+		 * "Define a list of accepted HTTP methods for the route"
+		 * 
+		 * limit_except GET POST DELETE;
+		 * 
+		 * (?)
+		 * bool	can_get;	|
+		 * bool	can_post;	|
+		 * bool	can_delete;	|-> by default all false
+		 * 
+		 */
+
+		/**
+		 * "Define a HTTP redirection"
+		 * 
+		 * return 301 /new_path;
+		 */
+		bool		is_redir;
+		int			status_code;
+		std::string	new_path;
+
+		bool		autoindex;
+
+		std::string	index;
+
 	public:
 };
 
